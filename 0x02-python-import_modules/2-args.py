bfb19c503dd5d867747f0d9df41ab22e42e8644e
admidere <admasud89@gmail.com>
2022-12-01 18:41:01 +0000

    imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{} argument{}{}".format(len(sys.argv) - 1, "" if len(sys.argv) - 1 == 1 else "s", "." if len(sys.argv) - 1 == 0 else ":"))
+    print("{} argument{}{}"
+        .format(len(sys.argv) - 1, "" if len(sys.argv) - 1 == 1 else "s", "." if len(sys.argv) - 1 == 0 else ":"))
     i = 1
     while i <= len(sys.argv) - 1:
         print("{}: {}".format(i, sys.argv[i]))
