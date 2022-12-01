9b4ddbfc5f758731d5ddb0fe749c466bc35b3008
admidere <admasud89@gmail.com>
2022-12-01 18:33:57 +0000

    imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    print("{} Arguments count:".format(len(sys.argv)))
-    for i, arg in enumerate((sys.argv), start = 1):
-        print("{}: {}".format(i, arg))
+    import sys
+    print("{} argument{}{}".format(len(sys.argv) - 1,
+    "" if len(sys.argv) - 1 == 1
+    else "s",
+    "." if len(sys.argv) - 1 == 0
+    else ":"))
+
+    i = 1
+    while i <= len(sys.argv) - 1:
+        print("{}: {}".format(i, sys.argv[i]))
+        i += 1
