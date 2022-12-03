0a066a86b7ecf7e605fb8c8942efc49c17b4098f
admidere <admasud89@gmail.com>
2022-12-03 16:17:41 +0000

    list intigers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    print("{}".format(my_list[0]))
-    print("{}".format(my_list[1]))
-    print("{}".format(my_list[2]))
-    print("{}".format(my_list[3]))
-    print("{}".format(my_list[4]))
+    print("{:d}".format(my_list[0]))
+    print("{:d}".format(my_list[1]))
+    print("{:d}".format(my_list[2]))
+    print("{:d}".format(my_list[3]))
+    print("{:d}".format(my_list[4]))
