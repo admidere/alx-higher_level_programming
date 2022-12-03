dba5cf80a8a70f6b7fd2f33956200e3a056c4367
admidere <admasud89@gmail.com>
2022-12-03 19:27:52 +0000

    list intigers

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,5 +7,6 @@ def new_in_list(my_list, idx, element):
         return new_list
     for i in new_list:
         if i == idx:
+            new_list[i] = []
             new_list[i] = element
-            return new_list
+    return new_list
