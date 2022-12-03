318dc01ca441adf257d3dc5a5104349c693aa3e2
admidere <admasud89@gmail.com>
2022-12-03 18:07:35 +0000

    list intigers

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    len = 0
+    for i in my_list:
+        len += 1
     if idx < 0:
         return my_list
-    if idx > len(my_list):
+    if idx > len - 1:
         return my_list
-    my_list.pop(idx)
-    my_list.insert(idx, element)
-    return (my_list)
+    for i in range(len):
+        if i == idx:
+            my_list[i] = []
+            my_list[i] = element
+        return my_list
