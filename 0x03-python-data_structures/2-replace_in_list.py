5c13bc6c9afb5a243b928cb7a0d7acf4642f2a60
admidere <admasud89@gmail.com>
2022-12-03 17:59:26 +0000

    list intigers

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,6 +6,5 @@ def replace_in_list(my_list, idx, element):
         return my_list
     for i in range(len(my_list)):
         if i == idx:
-            my_list[i] = []
             my_list[i] = element
-            return my_list
+        return my_list
