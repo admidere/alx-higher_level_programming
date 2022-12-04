9bef82d5d2a43e04dbfc47be890ae2ef27025e80
admidere <admasud89@gmail.com>
2022-12-04 21:21:03 +0000

    tuple

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            print(matrix[i][j], end=' ')
-        print()
+    for i in matrix:
+        print("{}".format(" ".join(map(str, i))))
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    i = len(tuple_a)
+    j = len(tuple_b)
+    if i >= 2 and j >= 2:
+        return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    elif i >= 2 and j == 1:
+        return tuple_a[0] + tuple_b[0], tuple_a[1]
+    elif i >= 2 and j == 0:
+        return tuple_a[0], tuple_a[1]
+    elif i == 0 and j == 0:
+        return 0, 0
+    elif i == 1 and j >= 2:
+        return tuple_a[0] + tuple_b[0], tuple_b[1]
+    elif i == 0 and j >= 2:
+        return tuple_b[0], tuple_b[1]
+    elif i == 1 and j == 1:
+        return tuple_a[0] + tuple_b[0], 0
+    elif i == 1 and j == 0:
+        return tuple_a[0], 0
+    elif i == 0 and j == 1:
+        return tuple_b[0], 0
