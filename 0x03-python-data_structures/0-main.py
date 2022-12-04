ce6dc479972a80ca34e618cb78cfd8c342a34327
admidere <admasud89@gmail.com>
2022-12-04 23:04:20 +0000

    tuple

--- 0x03-python-data_structures/0-main.py
+++ 0x03-python-data_structures/0-main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-max_integer = __import__('9-max_integer').max_integer
+delete_at = __import__('11-delete_at').delete_at
 
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
