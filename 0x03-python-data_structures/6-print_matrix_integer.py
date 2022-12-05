070b75bf60c03c641131e617fdd90b2169be7da2
admidere <admasud89@gmail.com>
2022-12-05 14:45:26 +0000

    switch

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return None
-    for i in matrix:
-        for j in i:
-            if j == i[-1]:
-                A = ""
-            else:
-                A = " "
-            print("{:d}".format(j), end = A)
-        print()
+    for submat in matrix:
+        print(" ".join("{:d}".format(elmt) for elmt in submat))
