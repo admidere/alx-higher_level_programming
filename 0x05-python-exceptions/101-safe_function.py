0f03625b37288bd483d019a5e4179d28a93fa1f2
admidere <admasud89@gmail.com>
2022-12-19 21:59:08 +0000

    exception

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        return(fct(*args))
+    except Exception as i:
+        print("Exception: {}".format(i), file=sys.stderr)
+        return None
