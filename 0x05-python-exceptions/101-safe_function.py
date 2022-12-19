7ff973dbc3b7201bd3983cab3a14e546995d28bc
admidere <admasud89@gmail.com>
2022-12-19 22:02:04 +0000

    exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        return(fct(*args))
+        return fct(*args)
     except Exception as i:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
