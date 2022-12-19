9cfc25299f27fd0c85a23d9323ffec71336327ab
admidere <admasud89@gmail.com>
2022-12-19 16:20:42 +0000

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             j += 1
-        except:
+        except Exception:
             pass
     print()
     return (j)
