15cd7ec2447de0cd0a52f1829c2e971aece1bf2d
admidere <admasud89@gmail.com>
2022-12-20 22:47:29 +0000

    class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""an empty class Square that defines a square"""
+"""class Square that defines a square"""
 
 class Square:
-    """defines a square"""
+    """Define a class square"""
     pass
