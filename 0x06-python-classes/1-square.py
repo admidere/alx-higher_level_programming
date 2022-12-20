07fdef5cdbb17610f9dd31b5f59d63dff516f7af
admidere <admasud89@gmail.com>
2022-12-20 23:06:10 +0000

    class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""square module class"""
+
+
+class Square:
+    """square class that contain all function
+    and object with in class"""
+    def __init__(self, size):
+        """initialization of a given argument with self
+        followed by dot and parameter"""
+        self.__size = size
