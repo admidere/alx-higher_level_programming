eda7f940b3c23ed8f42099a91875063d0fe1f2ad
admidere <admasud89@gmail.com>
2022-12-21 01:13:12 +0000

    class

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """square with private instance attribute size"""
+    def __init__(self, size=0):
+        """Lenght of the side of a square"""
+        self.size = size
+
+    @property
+    def size(self):
+        """current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setters are the methods"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """public instance method returns current sqr area"""
+        return self.__size ** 2
+    def my_print(self):
+        """prints the square to stdout with char #"""
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print("#", end="")
+            print()
