eab339c6b87f787f712b22040452756fd7ea6dd3
admidere <admasud89@gmail.com>
2022-12-21 01:22:55 +0000

    class

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,35 +3,56 @@
 
 
 class Square:
-    """square with private instance attribute size"""
-    def __init__(self, size=0):
-        """Lenght of the side of a square"""
+    """Represent a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
         self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """current size of the square."""
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """setters are the methods"""
-
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
-        """public instance method returns current sqr area"""
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """prints the square to stdout with char #"""
+        """Print the square with the # character."""
         if self.__size == 0:
-            print()
-        for i in range(self.__size):
-            for i in range(self.__size):
-                print("#", end="")
-            print()
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
