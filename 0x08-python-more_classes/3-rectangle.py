b815e501462feb264245349bb5a1240d0158c135
admidere <admasud89@gmail.com>
2023-01-02 04:06:59 -0800

    more class

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -55,6 +55,6 @@ class Rectangle:
             for i in range(self.__height):
                 for j in range(self.__width):
                     rect += "#"
-                if j == self.__width - 1:
+                if i != self.__height - 1:
                     rect += "\n"
             return rect
