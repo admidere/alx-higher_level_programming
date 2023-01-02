cd19f0cba0827736dada933b335b360f53413899
admidere <admasud89@gmail.com>
2023-01-02 03:59:01 -0800

    more class

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -48,12 +48,13 @@ class Rectangle:
 
     def __str__(self):
         """return string"""
-        if self.__width == 0 or self.__height == 0:
-            return ""
         rect = ""
-        for i in range(self.__height):
-            for j in range(self.__width):
-                rect += "#"
-            if j == self.__width - 1:
-                rect += "\n"
-        return rect
+        if self.__width == 0 or self.__height == 0:
+            return rect
+        else:
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    rect += "#"
+                if j == self.__width - 1:
+                    rect += "\n"
+            return rect
