4a3dfd8dfc81e70575efac36ffd522c80fd6dd30
admidere <admasud89@gmail.com>
2023-01-02 13:38:18 -0800

    more class

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -2,34 +2,34 @@
 """Program to solve the N queens puzzle challenge"""
 
 
-def isSafe(board, row, column):
+def isSafe(board, row, col):
     '''Checks if position is safe from attack.
     Args:
         board: The board state.
         row: The row to check.
         col: The column to check.
     '''
-    for c in range(column):
-        if board[c] is row or abs(board[c] - row) is abs(c - column):
+    for c in range(col):
+        if board[c] is row or abs(board[c] - row) is abs(c - col):
             return False
     return True
 
 
-def checkBoard(board, column):
+def checkBoard(board, col):
     '''Checks the board state column by column using backtracking.
     Args:
         board: The board state.
         col: The current colum to check.
     '''
     n = len(board)
-    if column is n:
+    if col is n:
         print(str([[c, board[c]] for c in range(n)]))
         return
 
     for row in range(n):
-        if isSafe(board, row, column):
-            board[column] = row
-            checkBoard(board, column + 1)
+        if isSafe(board, row, col):
+            board[col] = row
+            checkBoard(board, col + 1)
 
 
 if __name__ == "__main__":
@@ -47,5 +47,5 @@ if __name__ == "__main__":
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    board = [0 for column in range(n)]
+    board = [0 for col in range(n)]
     checkBoard(board, 0)
