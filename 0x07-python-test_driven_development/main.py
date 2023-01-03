fe7bccb2ce47de29e1e952634d557be310ab25f9
admidere <admasud89@gmail.com>
2023-01-03 15:28:28 -0800

    doctest

--- 0x07-python-test_driven_development/main.py
+++ 0x07-python-test_driven_development/main.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-print_square = __import__('4-print_square').print_square
+max_integer = __import__('6-max_integer').max_integer
 
-print_square(4)
-print("")
-print_square(10)
-print("")
-print_square(0)
-print("")
-print_square(1)
-print("")
-try:
-    print_square(-1)
-except Exception as e:
-    print(e)
-print("")
+print(max_integer([1, 2, 3, 4]))
+print(max_integer([1, 3, 4, 2]))
