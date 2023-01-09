5cfd8a2121a77954c34009122f08c500ad4ea3d7
admidere <admasud89@gmail.com>
2023-01-09 09:48:03 -0800

    inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,5 +7,10 @@ class Square(Rectangle):
     """class inherit from class rectangle"""
     def __init__(self, size):
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
