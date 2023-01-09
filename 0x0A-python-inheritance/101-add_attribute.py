005ed3b2b2e3378532286dc0be0ef3e98d78ed67
admidere <admasud89@gmail.com>
2023-01-09 10:44:44 -0800

    inheritance

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module"""
+
+def add_attribute(cla, attri, value):
+    """Function that adds a new attribute to an object if it's possible"""
+    if not(hasattr(cla, '__dict__')):
+        raise TypeError("can't add new attribute")
+    setattr(cla, attri, value)
