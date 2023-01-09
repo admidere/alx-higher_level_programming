8713b57de7fb526c446365e0dddd466eb80af296
admidere <admasud89@gmail.com>
2023-01-09 06:36:33 -0800

    inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module"""
+
+
+def inherits_from(obj, a_class):
+    """returns:
+        True- if the object is an instance of a class that inherited (directly or indirectly)
+        from the specified class
+        false - otherwise False."""
+    return isinstance(obj, a_class) and type(obj) != a_class
