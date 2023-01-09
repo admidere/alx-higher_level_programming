f7dcb59cfdcf2aa187e6a35cb2c527c478dc0c12
admidere <admasud89@gmail.com>
2023-01-09 06:05:42 -0800

    inheritance

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-lookup = __import__('0-lookup').lookup
+is_same_class = __import__('2-is_same_class').is_same_class
 
-class MyClass1(object):
-    pass
-
-class MyClass2(object):
-    my_attr1 = 3
-    def my_meth(self):
-        pass
-
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
+a = 1
+if is_same_class(a, int):
+    print("{} is an instance of the class {}".format(a, int.__name__))
+if is_same_class(a, float):
+    print("{} is an instance of the class {}".format(a, float.__name__))
+if is_same_class(a, object):
+    print("{} is an instance of the class {}".format(a, object.__name__))
