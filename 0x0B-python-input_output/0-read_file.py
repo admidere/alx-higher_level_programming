00d7a77026f6c1bd6823f58a3707315497ac9263
admidere <admasud89@gmail.com>
2023-01-10 06:27:30 -0800

    in/output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """file open for read"""
-    with open("my_file_0.txt", "r", encoding="utf-8") as f:
+    with open("my_file_0.txt", encoding="utf-8") as f:
         print(f.read())
