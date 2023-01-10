278c5059e966e40aa8e590da0894fb26fad3e6b4
admidere <admasud89@gmail.com>
2023-01-10 13:04:24 -0800

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""simple module"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return
+    rows = [[1 for j in range(i + 1)] for i in range(n)]
+    for n in range(n):
+        for i in range(n - 1):
+            rows[n][i + 1] = sum(rows[n - 1][i:i + 2])
+    return rows
