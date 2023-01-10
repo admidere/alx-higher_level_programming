5b5516ea7b57d72f3197ce5f6abfdf5338c13279
admidere <admasud89@gmail.com>
2023-01-10 11:41:22 -0800

    added item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """module"""
 import json
-from sys import argv
+import sys
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
+args = sys.argv
 file_name = "add_item.json"
+# can also use os.path.exists(file) for try and except
+# be sure to import os.path
 
 try:
     json_list = load_from_json_file(file_name)
 except Exception:
     json_list = []
-for item in argv[1:]:
-    json_list.extend(item)
+for item in args[1:]:
+    json_list.append(item)
 
 save_to_json_file(json_list, file_name)
