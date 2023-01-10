9bbbc8c751ad9a6055f8afcc2106b0286f42d652
admidere <admasud89@gmail.com>
2023-01-10 10:57:41 -0800

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,11 +3,6 @@
 import json
 from sys import argv
 
-
-#!/usr/bin/python3
-"""Module for a script that adds all arguments to a Python list"""
-import json
-from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
@@ -16,7 +11,7 @@ file_name = "add_item.json"
 
 try:
     json_list = load_from_json_file(file_name)
-except:
+except Exception:
     json_list = []
 for item in argv[1:]:
     json_list.append(item)
