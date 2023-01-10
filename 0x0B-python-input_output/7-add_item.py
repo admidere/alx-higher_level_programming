be36ac863ade64ea21019c46914061096e47647b
admidere <admasud89@gmail.com>
2023-01-10 12:02:51 -0800

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """Module for a script that adds all arguments to a Python list"""
 from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+if __name__ == "__main__":
+    load_from_json_file =\
+        __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+    file_name = "add_item.json"
 
-file_name = "add_item.json"
+    # can also use os.path.exists(file) for try and except
+    # be sure to import os.path
+    try:
+        json_list = load_from_json_file(file_name)
+    except FileNotFoundError:
+        json_list = []
+    for item in argv[1:]:
+        json_list.append(item)
 
-# can also use os.path.exists(file) for try and except
-# be sure to import os.path
-try:
-    json_list = load_from_json_file(file_name)
-except:
-    json_list = []
-for item in argv[1:]:
-    json_list.append(item)
-
-save_to_json_file(json_list, file_name)
+    save_to_json_file(json_list, file_name)
