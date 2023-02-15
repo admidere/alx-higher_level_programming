7ada586faf3bcd26ba2b5691fccb923ccd573084
admidere <111120975+admidere@users.noreply.github.com>
2023-02-15 09:32:02 -0800

    creates db hbtn_9d_usa

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,8 @@
--- creates the database hbtn_0d_usa and the table states
--- (in the database hbtn_0d_usa) on your MySQL server.
+-- creates db hbtn_9d_usa
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
-            id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, 
-            name VARCHAR(256) NOT NULL)
+
+-- create table states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	name VARCHAR(256) NOT NULL);	
