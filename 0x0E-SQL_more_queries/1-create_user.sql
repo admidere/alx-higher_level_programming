fccfd67c2d2abf1db9aff9db85f85684cf3f56c0
admidere <111120975+admidere@users.noreply.github.com>
2023-02-15 04:08:00 -0800

    creates the MySQL server user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,6 @@
 -- creates the MySQL server user user_0d_1
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- user_0d_1 should have all privileges on your MySQL server
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
