2f42382fdf8661ed9d73afe6bf2d1187fb8045d3
admidere <admasud89@gmail.com>
2023-03-13 13:12:18 -0700

    javascript

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const index = Number.parseInt(process.argv[2], 10);
+if (isNaN(index)) {
+    console.log('Missing size');
+} else {
+  for (let i = 0; i < index; i++) {
+    let row = '';
+    for (let j = 0; j < index; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
