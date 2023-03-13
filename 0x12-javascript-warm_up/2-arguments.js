fd32c97aa811deabb48ebfabd36908bd50358fc5
admidere <admasud89@gmail.com>
2023-03-13 07:55:35 -0700

    javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const length = process.argv.length;
+if (length <= 2) {
+  console.log('No argument');
+} else if (length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
