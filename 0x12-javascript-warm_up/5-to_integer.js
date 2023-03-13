4168beb5061954ea7d558495acfe119aa0ef776b
admidere <admasud89@gmail.com>
2023-03-13 12:02:22 -0700

    javascript

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const number = Number.parseInt(process.argv[2], 10);
 if (!isNaN(number)) {
-  console.log('My number: ' + number);  
+  console.log('My number: ' + number);
 } else {
   console.log('Not a number');
 }
