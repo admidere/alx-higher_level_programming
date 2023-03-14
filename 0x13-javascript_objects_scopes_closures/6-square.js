29ed0b51e13c58452db03c15912c78b3d8f7f74b
admidere <111120975+admidere@users.noreply.github.com>
2023-03-14 14:44:40 -0700

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,14 +5,15 @@ class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
+  
   charPrint (c) {
-    if (c === undefined ) {
+    if (c === undefined) {
       super.print();
     } else {
       for (let i = 0; i < this.height; i++) {
         let mat = '';
         for (let j = 0; j < this.width; j++) {
-            mat += c;
+          mat += c;
         }
         console.log(mat);
       }
