2a99ef661ca9d9224b236729663dad3936534583
Admasu <admasud89@gmail.com>
2023-03-19 09:34:02 -0700

    changed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """This script takes 3 argument and lists all the states
 + the data base"""
-
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
-
     db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
     cr = db.cursor()
-
     cr.execute("SELECT states.id, states.name FROM states")
     [print(state) for state in cr.fetchall()]
+    cr.close()
+    db.close()
