e0fbd3e233b65c96925e82b79830308af1e33e1e
admidere <admasud89@gmail.com>
2023-03-30 20:49:20 +0000

    method

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Get the URL from the command line argu
-curl -sI -L -X OPTIONS -i "$1" 2>/dev/null | grep "Allow" | tr -d '\r\n'
+curl -sI -L -X OPTIONS "$1" | grep -w 'Allow' | cut -d ' ' -f2-
