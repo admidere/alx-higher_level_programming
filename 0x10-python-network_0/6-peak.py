fa7a293fadac31f3b24a057a5e1c43dfe0a4bbb2
admidere <111120975+admidere@users.noreply.github.com>
2023-03-30 14:48:40 -0700

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,25 +3,23 @@
 
 
 def find_peak(list_of_integers):
-    """
-    Finds a peak in a list of unsorted integers
-    """
+    """Finds the peak in an unsorted list of ints"""
+    if len(list_of_integers) > 1:
+        if list_of_integers[0] >= list_of_integers[1]:
+            return list_of_integers[0]
+        if list_of_integers[-1] >= list_of_integers[-2]:
+            return list_of_integers[-1]
+        return _find_peak(list_of_integers, 0, len(list_of_integers))
     if not list_of_integers:
         return None
+    return list_of_integers[0]
 
-    n = len(list_of_integers)
-    if n == 1:
-        return list_of_integers[0]
 
-    left = 0
-    right = n - 1
-
-    while left < right:
-        mid = (left + right) // 2
-
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            right = mid
-        else:
-            left = mid + 1
-
-    return list_of_integers[left]
+def _find_peak(lint, start, stop):
+    """Recursively finds peak"""
+    if stop - start < 2:
+        return None
+    mid = (start + stop) // 2
+    if lint[mid] >= lint[mid - 1] and lint[mid] >= lint[mid + 1]:
+        return lint[mid]
+    return _find_peak(lint, start, mid) or _find_peak(lint, mid, stop)
