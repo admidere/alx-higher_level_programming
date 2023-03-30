f773d9447d23ddebbf056cf6b5e18b5fe3815bb3
admidere <admasud89@gmail.com>
2023-03-30 20:44:00 +0000

    method

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Get the URL from the command line argument
-url=$1
-
-# Use curl to send a request to the server and get the response headers
-headers=$(curl -I -X OPTIONS $url 2>/dev/null)
-
-# Extract the Allow header from the response headers
-allow_header=$(echo "$headers" | grep -i allow)
+# Get the URL from the command line argu
+curl -X OPTIONS -i $1 2>/dev/null | grep "Allow" | tr -d '\r\n'
