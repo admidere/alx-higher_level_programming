d5af35e673134bedbdb62ac2829bb74e4b7cc8df
admidere <admasud89@gmail.com>
2023-03-30 16:54:48 +0000

    size of body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,13 +2,4 @@
 # takes in a URL, sends a request to that URL using curl, and
 # displays the size of the response body in bytes
 
-#!/bin/bash
-
-# get URL from command line argument
-url=$1
-
-# send request to URL and get response body size in bytes
-response_size=$(curl -s -o /dev/null -w '%{size_download}' "$url")
-
-# display response body size
-echo "$response_size"
+curl -so /dev/null -w '%{size_download}\n' $1
