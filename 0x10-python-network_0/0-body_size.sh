1080d27fb67ff394dcc3e4a167fd27d7a123d7c2
admidere <admasud89@gmail.com>
2023-03-30 19:33:24 +0000

    size of body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # Check if a URL was provided as an argument
-if [ -z "$1" ]; then
-	  echo "Usage: $0 <url>"
-	    exit 1
-fi
-
-# Send a request to the URL and get the size of the body
-size=$(curl -sI "$1" | awk '/Content-Length/ { print $2 }')
-
-# Display the size of the body in bytes
-echo "${size}"
+curl -sI "$1" | awk '/Content-Length/ { print $2 }'
