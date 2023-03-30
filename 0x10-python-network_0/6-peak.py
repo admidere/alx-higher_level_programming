617980b4909b6241a6e5feaebaece787e78a1c9a
admidere <111120975+admidere@users.noreply.github.com>
2023-03-30 14:43:17 -0700

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
-"""find peak"""
+"""Finds the peak in an unsorted list of integers"""
 
 def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers using a modified binary search algorithm.
+
+    Args:
+        list_of_integers (list): List of unsorted integers.
+
+    Returns:
+        int: A peak value from the list.
+
+    Raises:
+        ValueError: If the input list is empty.
+    """
     if not list_of_integers:
         return None
-    peak = 0
-    for i in range(len(list_of_integers)):
-        if list_of_integers[i] > peak:
-            peak = list_of_integers[i]
-    return peak
+
+    n = len(list_of_integers)
+    if n == 1:
+        return list_of_integers[0]
+
+    left = 0
+    right = n - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+
+    return list_of_integers[left]
