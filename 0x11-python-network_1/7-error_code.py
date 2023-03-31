b6cbdea4d7ec2ffbc570ad8913324aad05804801
admidere <admasud89@gmail.com>
2023-03-31 21:03:12 +0000

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,11 +8,11 @@ import sys
 
 
 if __name__ == '__main__':
-   url = sys.argv[1]
+    url = sys.argv[1]
 
-response = requests.get(url)
+    response = requests.get(url)
 
-if response.status_code >= 400:
-    print("Error code: " + str(response.status_code))
-else:
-    print(response.text)
+    if response.status_code >= 400:
+        print("Error code: " + str(response.status_code))
+    else:
+        print(response.text)
