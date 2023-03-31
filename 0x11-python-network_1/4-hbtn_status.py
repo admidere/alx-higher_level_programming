9c676297a1d3d095a17776b122da247cf403374d
admidere <admasud89@gmail.com>
2023-03-31 20:28:03 +0000

    my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,6 @@ if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
 
+    print('Body response:')
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
