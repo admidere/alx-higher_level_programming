f2e9483a4c5f7f85a8b85bd255e77413bcc84e4f
admidere <111120975+admidere@users.noreply.github.com>
2023-03-31 13:17:07 -0700

    my status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == '__main__':
+response = requests.get('https://alx-intranet.hbtn.io/status')
+data = response.json()
+
+print('Body response:')
+print('\t- type: {}'.format(type(data)))
+print('\t- content: {}'.format(data))
