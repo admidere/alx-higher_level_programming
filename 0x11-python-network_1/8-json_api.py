6cd33cf01bd7c8f85cd84644095942561882d531
admidere <admasud89@gmail.com>
2023-03-31 21:16:08 +0000

    search api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,27 +8,28 @@ import sys
 
 
 if __name__ == '__main__':
-# Set the URL to send the POST request to
-url = "http://0.0.0.0:5000/search_user"
+    # Set the URL to send the POST request to
+    url = "http://0.0.0.0:5000/search_user"
 
-# Get the letter as input, default to empty string if no argument is given
-q = sys.argv[1] if len(sys.argv) > 1 else ""
+    # Get the letter as input, default to empty string if no argument is given
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
 
-# Create the data payload for the POST request
-payload = {"q": q}
+    # Create the data payload for the POST request
+    payload = {"q": q}
 
-# Send the POST request with the data payload
-response = requests.post(url, data=payload)
+    # Send the POST request with the data payload
+    response = requests.post(url, data=payload)
 
-try:
-    # Try to parse the response body as JSON
-    json_response = response.json()
+    try:
+        # Try to parse the response body as JSON
+        json_response = response.json()
 
-    # If the JSON is properly formatted and not empty, display the id and name
-    if json_response:
-        print("[{}] {}".format(json_response.get("id"), json_response.get("name")))
-    else:
-        print("No result")
-except ValueError:
-    # If the response body is not properly formatted JSON, display an error message
-    print("Not a valid JSON")
+        # If the JSON is properly formatted and not emptty
+        if json_response:
+            print("[{}] {}".format(json_response.get("id"),
+                  json_response.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        # If the response body is not properly formatted JSon
+        print("Not a valid JSON")
