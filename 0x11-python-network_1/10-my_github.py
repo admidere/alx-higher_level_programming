3f9d4f5a9491d43bb8f28aa644d833f99beef5c2
admidere <admasud89@gmail.com>
2023-03-31 21:35:14 +0000

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,22 +9,22 @@ import sys
 
 
 if __name__ == '__main__':
-username = sys.argv[1]
-pat = sys.argv[2]
+    username = sys.argv[1]
+    pat = sys.argv[2]
 
-# Construct the API endpoint URL
-url = f"https://api.github.com/users/{username}"
+    # Construct the API endpoint URL
+    url = f"https://api.github.com/users/{username}"
 
-# Set the Authorization header with the Basic Authentication token
-headers = {"Authorization": f"Basic {username}:{pat}"}
+    # Set the Authorization header with the Basic Authentication token
+    headers = {"Authorization": f"Basic {username}:{pat}"}
 
-# Make the API request
-response = requests.get(url, headers=headers)
+    # Make the API request
+    response = requests.get(url, headers=headers)
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Extract the user ID from the response JSON
-    user_id = response.json()["id"]
-    print(f"Your user ID is {user_id}")
-else:
-    print("Error:", response.status_code)
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Extract the user ID from the response JSON
+        user_id = response.json()["id"]
+        print(user_id)
+    else:
+        print("Error:", response.status_code)
