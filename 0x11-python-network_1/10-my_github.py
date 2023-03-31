205b0075a9817d48470d679b85e746b6df85e4d7
admidere <111120975+admidere@users.noreply.github.com>
2023-03-31 14:41:22 -0700

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,22 +9,22 @@ import sys
 
 
 if __name__ == '__main__':
-    username = sys.argv[1]
-    pat = sys.argv[2]
 
-    # Construct the API endpoint URL
-    url = f"https://api.github.com/users/{username}"
+# Replace with your GitHub username and personal access token
+username = sys.argv[1]
+password = sys.argv[2]
 
-    # Set the Authorization header with the Basic Authentication token
-    headers = {"Authorization": f"Basic {username}:{pat}"}
+# Set up the API endpoint and headers
+api_endpoint = 'https://api.github.com/user'
+headers = {'Accept': 'application/vnd.github.v3+json'}
 
-    # Make the API request
-    response = requests.get(url, headers=headers)
+# Make the API request using Basic Authentication
+response = requests.get(api_endpoint, auth=(username, password), headers=headers)
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Extract the user ID from the response JSON
-        user_id = response.json()["id"]
-        print(user_id)
-    else:
-        print("Error:", response.status_code)
+# Check if the request was successful
+if response.status_code == 200:
+    # Extract and display the user id
+    user_id = response.json()['id']
+    print(f"Your GitHub user id is {user_id}.")
+else:
+    print("Failed to retrieve your user id.")
