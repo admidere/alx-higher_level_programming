81f4b5e993cac2afebd2e9e1569ff50bc9a88d8a
admidere <111120975+admidere@users.noreply.github.com>
2023-03-31 14:10:42 -0700

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+# Set the URL to send the POST request to
+url = "http://0.0.0.0:5000/search_user"
+
+# Get the letter as input, default to empty string if no argument is given
+q = sys.argv[1] if len(sys.argv) > 1 else ""
+
+# Create the data payload for the POST request
+payload = {"q": q}
+
+# Send the POST request with the data payload
+response = requests.post(url, data=payload)
+
+try:
+    # Try to parse the response body as JSON
+    json_response = response.json()
+
+    # If the JSON is properly formatted and not empty, display the id and name
+    if json_response:
+        print("[{}] {}".format(json_response.get("id"), json_response.get("name")))
+    else:
+        print("No result")
+except ValueError:
+    # If the response body is not properly formatted JSON, display an error message
+    print("Not a valid JSON")
