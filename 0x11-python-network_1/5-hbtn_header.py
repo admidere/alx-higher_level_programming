3143299906dea6287286ccac779292e102cadd83
admidere <admasud89@gmail.com>
2023-03-31 20:42:59 +0000

    header value response

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,12 +4,13 @@ Python script that takes in a URL, sends a request to the URL and
 displays the value of the variable X-Request-Id in the response header
 """
 import sys
+import requests
 
 
 if __name__ == '__main__':
-url = sys.argv[1]
+    url = sys.argv[1]
 
-response = requests.get(url)
-request_id = response.headers.get('X-Request-Id')
+    response = requests.get(url)
+    request_id = response.headers.get('X-Request-Id')
 
-print(f'The value of X-Request-Id is {request_id}')
+    print(request_id)
