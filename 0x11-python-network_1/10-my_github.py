d506c900ccf77f4c8dcce9ed0f5ec408f4698d84
admidere <admasud89@gmail.com>
2023-03-31 21:45:04 +0000

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,21 +10,22 @@ import sys
 
 if __name__ == '__main__':
 
-# Replace with your GitHub username and personal access token
-username = sys.argv[1]
-password = sys.argv[2]
+    # Replace with your GitHub username and personal access token
+    username = sys.argv[1]
+    password = sys.argv[2]
 
-# Set up the API endpoint and headers
-api_endpoint = 'https://api.github.com/user'
-headers = {'Accept': 'application/vnd.github.v3+json'}
+    # Set up the API endpoint and headers
+    api_endpoint = 'https://api.github.com/user'
+    headers = {'Accept': 'application/vnd.github.v3+json'}
 
-# Make the API request using Basic Authentication
-response = requests.get(api_endpoint, auth=(username, password), headers=headers)
+    # Make the API request using Basic Authentication
+    response = requests.get(api_endpoint, auth=(username, password),
+                            headers=headers)
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Extract and display the user id
-    user_id = response.json()['id']
-    print(f"Your GitHub user id is {user_id}.")
-else:
-    print("Failed to retrieve your user id.")
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Extract and display the user id
+        user_id = response.json()['id']
+        print(user_id)
+    else:
+        print("None")
