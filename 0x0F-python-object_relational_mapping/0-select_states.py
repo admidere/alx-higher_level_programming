72658ef5702a6c1b7af76740a203ce9eafd6fd74
admidere <admasud89@gmail.com>
2023-04-19 16:53:33 -0700

    select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""script that lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
@@ -6,7 +8,6 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=db_name)
     cur = db.cursor()
