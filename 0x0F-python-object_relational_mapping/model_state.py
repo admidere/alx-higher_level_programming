93e3ac888e01d162bf4c06e093066229df027ac3
admidere <111120975+admidere@users.noreply.github.com>
2023-04-20 06:35:11 -0700

    model_state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+ python file that contains the class definition of a State
+ and an instance Base = declarative_base():
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+# Create the Base instance
+Base = declarative_base()
+
+# Define the State class
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+# Connect to the MySQL server
+engine = create_engine('mysql://localhost:3306')
+
+# Create the table
+Base.metadata.create_all(engine)
