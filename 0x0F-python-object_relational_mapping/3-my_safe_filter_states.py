d7ed69ab015f3d2f31b03ba2a2b84853d5201204
admidere <111120975+admidere@users.noreply.github.com>
2023-04-19 18:00:53 -0700

    my_safe_filter_states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""script that lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    # Get the MySQL username, password, and database name
+    # from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    searched_name = sys.argv[4]
+
+    # Connect to the MySQL server running on localhost at port 3306
+    conn = MySQLdb.connect(host='localhost', port=3306, user=username,
+                           passwd=password, db=database)
+
+    # Prepare a parameterized query with a placeholder for the user input
+    query = "SELECT * FROM states WHERE BINARY name LIKE %s ORDER BY id ASC"
+
+    # Execute the parameterized query with the user input as a tuple
+    cursor = conn.cursor()
+    cursor.execute(query, (searched_name + '%',))
+
+    # Fetch all the rows returned by the query
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and the connection
+    cursor.close()
+    conn.close()
