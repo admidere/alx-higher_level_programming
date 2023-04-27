708d68967ad5362e2ca82e867870485edef951a6
admidere <admasud89@gmail.com>
2023-04-27 13:49:18 -0700

    updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """bash script thart finds peak from list unsorted array"""
 
+
 def find_peak(list_of_integers):
     if not list_of_integers:
         return None
-    
+
     left = 0
     right = len(list_of_integers) - 1
-    
+
     while left < right:
         mid = (left + right) // 2
         if list_of_integers[mid] < list_of_integers[mid+1]:
             left = mid + 1
         else:
             right = mid
-    
+
     return list_of_integers[left]
