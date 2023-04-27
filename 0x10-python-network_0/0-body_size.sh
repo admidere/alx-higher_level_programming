3e95e6cd4aa2415b374f0fd35a87ecdc186034d9
admidere <admasud89@gmail.com>
2023-04-27 02:40:31 -0700

    determined body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL
-# band displays the size of the body of the response
-
-# Check that an argument was provided
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send a request to the URL and save the response body
-response=$(curl -s $1)
-
-# Get the size of the response body in bytes
-size=$(echo -n $response | wc -c)
-
-# Display the size of the response body
-echo "$size"
+# This script takes in a URL, sends a request to that URL using curl, and displays the size of the body of the response in bytes
+curl -s "$1" | wc -c
