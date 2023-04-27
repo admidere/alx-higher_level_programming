16e9f67cef320a9cb14b2e52f9235f5baf16db3f
admidere <admasud89@gmail.com>
2023-04-27 14:19:01 -0700

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to the server and causes it to respond with "You got me!"
-curl -s -X PUT -H "Content-Type: text/plain" -d "You got me!" "0.0.0.0:5000/catch_me" -w "\n"
+# This script makes a request to catch_me endpoint and causes the server to respond with "You got me!" message.
+curl -s -X PUT -H "Content-Type: text/plain" -d "user_id=42" 0.0.0.0:5000/catch_me --output /dev/null
