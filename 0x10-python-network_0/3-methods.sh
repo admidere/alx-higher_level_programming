936ec7262154c18d5ff230028fed59689b56e25f
admidere <admasud89@gmail.com>
2023-04-27 06:57:06 -0700

    get response method

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL and displays all HTTP methods the server will accept
-curl -I -X OPTIONS $1 | grep "Allow" | cut -d " " -f2-
+curl -I -X OPTIONS "$1" | grep "Allow" | cut -d " " -f2-
