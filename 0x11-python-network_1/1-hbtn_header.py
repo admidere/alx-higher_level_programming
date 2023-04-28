097206eb57fecf32d1c2be516d567ded016fc8a3
admidere <admasud89@gmail.com>
2023-04-28 07:10:59 -0700

    get header id response

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL and
+displays the value of the X-Request-Id variable found in the
+header of the response."""
+from sys import argv
+import urllib.request
+
+# get a url from CLA
+url = argv[1]
+
+# use with statment for open request url and get response the header id
+with urllib.request.urlopen(url) as response:
+    # get the value of requested id and store at id variable
+    id = response.headers.get('X-Request-Id')
+print(id)
