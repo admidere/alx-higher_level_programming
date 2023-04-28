8ecc95b3df62934a15c2c932a4e457ecda71a8ef
admidere <admasud89@gmail.com>
2023-04-28 12:04:59 -0700

    github commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,23 +4,18 @@ a back-end position with multiple technical challenges"""
 import requests
 import sys
 
-if __name__ = '__main__':
-    # Set the GitHub API endpoint
-    endpoint = "https://api.github.com/repos/{}/{}/commits"
-
-    # Get the repository name and owner name from the command-line arguments
+if __name__ == "__main__":
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
 
-    # Send a GET request to the endpoint with the repository and owner names
-    response = requests.get(endpoint.format(owner_name, repo_name))
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
 
-    # If the response is successful, print the last 10 commits
+    response = requests.get(url)
     if response.status_code == 200:
-        commits = response.json()[:10]  # Get the last 10 commits
-        for commit in commits:
+        commits = response.json()
+        for commit in commits[:10]:
             sha = commit["sha"]
             author_name = commit["commit"]["author"]["name"]
-            print("{}".format(author_name))
+            print(f"{sha}: {author_name}")
     else:
         print("Error")
