ee25f8de127c912ac239eebfc0ac1e33e30b905e
admidere <admasud89@gmail.com>
2023-04-28 11:50:28 -0700

    github commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -21,6 +21,6 @@ if __name__ = '__main__':
         for commit in commits:
             sha = commit["sha"]
             author_name = commit["commit"]["author"]["name"]
-            print(f"{sha}: {author_name}")
+            print("{}".format(author_name))
     else:
         print("Error")
