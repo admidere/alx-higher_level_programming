f6b87fc9f21fe4063e6d1c48a9a6931f9308f1c6
admidere <admasud89@gmail.com>
2023-04-28 07:20:06 -0700

    get header id response

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,13 +5,14 @@ header of the response."""
 import urllib.request
 import sys
 
-# Get URL from command line argument
-url = sys.argv[1]
+if __name__ == '__main__':
+    # Get URL from command line argument
+    url = sys.argv[1]
 
-# Send request to URL and get response
-with urllib.request.urlopen(url) as response:
-    # Get the value of the X-Request-Id variable from the response header
-    x_request_id = response.headers.get('X-Request-Id')
+    # Send request to URL and get response
+    with urllib.request.urlopen(url) as response:
+        # Get the value of the X-Request-Id variable from the response header
+        x_request_id = response.headers.get('X-Request-Id')
 
-    # Display the value of the X-Request-Id variable
+        # Display the value of the X-Request-Id variable
     print(x_request_id)
