f6a5246d25720d4dc45e84e846ed434738ade3bf
admidere <admasud89@gmail.com>
2023-04-28 09:21:29 -0700

    get url and header response

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,14 +4,15 @@ the variable X-Request-Id in the response header"""
 import requests
 import sys
 
-# Get the URL from the command-line arguments
-url = sys.argv[1]
+if __name__ == '__main__':
+    # Get the URL from the command-line arguments
+    url = sys.argv[1]
 
-# Send a GET request to the URL
-response = requests.get(url)
+    # Send a GET request to the URL
+    response = requests.get(url)
 
-# Read the value of the X-Request-Id header from the response
-x_request_id = response.headers.get('X-Request-Id')
+    # Read the value of the X-Request-Id header from the response
+    x_request_id = response.headers.get('X-Request-Id')
 
-# Print the value of the X-Request-Id header to the terminal
-print(x_request_id)
+    # Print the value of the X-Request-Id header to the terminal
+    print(x_request_id)
