ac8d72683a98faf07ca992b5759499091ea92f2e
admidere <admasud89@gmail.com>
2023-05-23 06:46:13 -0700

    write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,17 +2,13 @@
 
 const fs = require('fs');
 
-function writeToFile(filePath, content) {
-  fs.writeFile(filePath, content, 'utf-8', (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(`Successfully wrote to file: ${filePath}`);
-    }
-  });
-}
-
-// Usage: node script.js <file_path> <content>
 const filePath = process.argv[2];
 const content = process.argv[3];
-writeToFile(filePath, content);
+
+fs.writeFile(filePath, content, 'utf8', (error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('File written successfully');
+  }
+});
