c30caa50e0e10c044682e8aee5226a06cc069a80
admidere <admasud89@gmail.com>
2023-05-23 12:35:29 -0700

    101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,51 +1,22 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-function printMovieCharacters (movieId) {
-  const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-  request.get(filmUrl, (error, response, body) => {
-    if (error) {
-      console.error(error);
-    } else if (response.statusCode !== 200) {
-      console.error(`Failed to fetch movie data. Status code: ${response.statusCode}`);
-    } else {
-      const film = JSON.parse(body);
-      const characters = film.characters;
-
-      if (characters.length === 0) {
-        console.log('No characters found for this movie.');
-      } else {
-        let count = 0;
-
-        function fetchCharacter (characterUrl) {
-          request.get(characterUrl, (error, response, body) => {
-            if (error) {
-              console.error(error);
-            } else if (response.statusCode !== 200) {
-              console.error(`Failed to fetch character data. Status code: ${response.statusCode}`);
-            } else {
-              const character = JSON.parse(body);
-              console.log(character.name);
-            }
-
-            count++;
-
-            if (count === characters.length) {
-              process.exit();
-            }
-          });
-        }
-
-        characters.forEach((characterUrl) => {
-          fetchCharacter(characterUrl);
-        });
-      }
+function printCharacters (characters, index) {
+  if (index === characters.length) {
+    return;
+  }
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
     }
+    printCharacters(characters, index + 1);
   });
 }
 
-// Usage: node script.js <movie_id>
-const movieId = process.argv[2];
-printMovieCharacters(movieId);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
