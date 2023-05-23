c378045e761dbda5dff866a031e2abf1c96f7136
admidere <admasud89@gmail.com>
2023-05-23 12:28:48 -0700

    101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,51 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
+function printMovieCharacters (movieId) {
+  const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+  request.get(filmUrl, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else if (response.statusCode !== 200) {
+      console.error(`Failed to fetch movie data. Status code: ${response.statusCode}`);
+    } else {
+      const film = JSON.parse(body);
+      const characters = film.characters;
+
+      if (characters.length === 0) {
+        console.log('No characters found for this movie.');
+      } else {
+        let count = 0;
+
+        function fetchCharacter (characterUrl) {
+          request.get(characterUrl, (error, response, body) => {
+            if (error) {
+              console.error(error);
+            } else if (response.statusCode !== 200) {
+              console.error(`Failed to fetch character data. Status code: ${response.statusCode}`);
+            } else {
+              const character = JSON.parse(body);
+              console.log(character.name);
+            }
+
+            count++;
+
+            if (count === characters.length) {
+              process.exit();
+            }
+          });
         }
-      });
+
+        characters.forEach((characterUrl) => {
+          fetchCharacter(characterUrl);
+        });
+      }
     }
-  }
-});
+  });
+}
+
+// Usage: node script.js <movie_id>
+const movieId = process.argv[2];
+printMovieCharacters(movieId);
