5b579924de4aff9665198cbe148b582d4dc0a500
admidere <admasud89@gmail.com>
2023-05-31 11:18:36 -0700

    jquery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+// script that updates the text color of the <header> element to red (#FF0000)
+// when the user clicks on the tag DIV#red_header
+$(document).ready(function() {
+    $('header').css('color', '#FF0000');
+  });
+  
