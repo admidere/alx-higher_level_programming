4a91ad203c808bb5d4ea0fc03d8025c7b08a572c
admidere <111120975+admidere@users.noreply.github.com>
2023-08-05 05:20:57 -0700

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/13-add.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/node
-module.exports.add = (a, b) => (a + b);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-let i = 0;
-const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-while (i < array.length) {
-  console.log(array[i]);
-  i++;
-}
